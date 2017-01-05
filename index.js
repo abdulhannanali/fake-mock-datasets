b014ef3af61f7b245f9f7327ed870138092bfc2e
Hannan Ali <hannan@qubit.com>
2017-01-05 23:40:31 +0500

    remote express cache

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const redis = require('redis')
 const REDIS_URL = process.env.REDIS_URL
 const redisClient = redis.createClient(REDIS_URL)
 const serverData = require('./json-server/index.js')()
-const cache = require('express-redis-cache')({ client: redisClient, expire: 5 * 60 })
 
 const jsonRouter = jsonServer.router(serverData)
 
@@ -16,8 +15,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(jsonServer.defaults())
-
-app.get(cache.route(), jsonRouter)
 app.use(jsonRouter)
 
 app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0', function (error) {
@@ -25,5 +22,3 @@ app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0', function (er
         console.log('server is listening')
     }
 })
-
-cache.on('message', console.log)
