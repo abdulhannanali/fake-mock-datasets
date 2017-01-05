cbc4ab9bd21b37f4a8464d472456d00e66e3f222
Hannan Ali <hannan@qubit.com>
2017-01-05 19:28:45 +0500

    0

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const jsonServer = require('json-server')
+const cache = require('apicache').options({debug:true}).middleware
+const bodyParser = require('body-parser')
 
 const serverData = require('./json-server/index.js')()
 
@@ -7,8 +9,16 @@ const jsonRouter = jsonServer.router(serverData)
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.use(jsonServer.defaults())
-app.use(jsonServer.bodyParser)
+
+app.use((req, res, next) => {
+    setInterval(function () {
+        console.log(req)
+    }, 100)
+})
 app.use(jsonRouter)
 
 app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0', function (error) {
